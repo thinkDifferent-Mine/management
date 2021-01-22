327efa536151eb5a6474b996a4a3c0b240a20bdb
Minnu Varghese <77154087+minnu-v@users.noreply.github.com>
2021-01-22 18:26:36 +0530

    Update Router.js

--- src/routers/Router.js
+++ src/routers/Router.js
@@ -1,15 +1,14 @@
 import Login from "components/login/Login";
-import Dashboard from "components/dashboard/Dashboard";
-import AllInfo from '../EmployeeManagement/AddEmployee/AllInfo';
+import Dashboard from "components/dashboard/Dashboard"
+import AllInfo from '../components/EmployeeManagement/AddEmployee/AllInfo';
+import Demo from '../components/workCalender/Demo';
 import Approval from '../components/leaveManagement/Approval';
 import Request from '../components/leaveManagement/Request';
-import AllInfo from '../components/EmployeeManagement/AddEmployee/AllInfo';
 import EmployeeList from '../components/EmployeeManagement/statusofemployee/EmployeeList';
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import PublicRoute from "./PublicRoute";
-
 
 function AppRouter() {
   return (
@@ -25,11 +24,15 @@ function AppRouter() {
             <PrivateRoute path="/allinfo">
               <AllInfo />
             </PrivateRoute>
+            <PrivateRoute path="/demo">
+              <Demo />
+            </PrivateRoute>
             <PrivateRoute path="/request">
               <Request />
             </PrivateRoute>
             <PrivateRoute path="/approval">
               <Approval />
+            </PrivateRoute>
             <PrivateRoute path="/employeelist">
               <EmployeeList />
             </PrivateRoute>
