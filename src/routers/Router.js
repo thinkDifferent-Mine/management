d0c61002c3eb70ccabc5e478289b07ab82924c51
Minnu Varghese <77154087+minnu-v@users.noreply.github.com>
2021-01-22 18:01:16 +0530

    .

--- src/routers/Router.js
+++ src/routers/Router.js
@@ -1,13 +1,10 @@
 import Login from "components/login/Login";
 import Dashboard from "components/dashboard/Dashboard";
-<<<<<<< HEAD
 import AllInfo from '../EmployeeManagement/AddEmployee/AllInfo';
 import Approval from '../components/leaveManagement/Approval';
 import Request from '../components/leaveManagement/Request';
-=======
 import AllInfo from '../components/EmployeeManagement/AddEmployee/AllInfo';
 import EmployeeList from '../components/EmployeeManagement/statusofemployee/EmployeeList';
->>>>>>> a8f0d209353dfe3d1a37a03be4cab79d32cd687f
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
@@ -28,16 +25,13 @@ function AppRouter() {
             <PrivateRoute path="/allinfo">
               <AllInfo />
             </PrivateRoute>
-<<<<<<< HEAD
             <PrivateRoute path="/request">
               <Request />
             </PrivateRoute>
             <PrivateRoute path="/approval">
               <Approval />
-=======
             <PrivateRoute path="/employeelist">
               <EmployeeList />
->>>>>>> a8f0d209353dfe3d1a37a03be4cab79d32cd687f
             </PrivateRoute>
         </Switch>
       </Router>
